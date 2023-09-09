37768e6922b0bf81685e2a003d606181d1d78eba
SuperUserNameMan <yoann@terminajones.com>
2023-09-09 18:46:49 +0200

    autodetects `RL_SUPPORT_IMAGE_EXPORT/GENERATION/MANIPULATION`

--- raylib.4.5.ffi.php
+++ raylib.4.5.ffi.php
@@ -255,14 +255,14 @@ define_default( 'RL_SUPPORT_FILEFORMAT_PVR'  , str_contains( $_RAYLIB_CONTENT , 
 //------------------------------------------------------------------------------------
 
 // Support image export functionality (.png, .bmp, .tga, .jpg, .qoi)
-define_default( 'RL_SUPPORT_IMAGE_EXPORT'       , true );
+define_default( 'RL_SUPPORT_IMAGE_EXPORT'       , str_contains( $_RAYLIB_CONTENT , '%s_FORMAT' ) );
 
 // Support procedural image generation functionality (gradient, spot, perlin-noise, cellular)
-define_default( 'RL_SUPPORT_IMAGE_GENERATION'   , true );
+define_default( 'RL_SUPPORT_IMAGE_GENERATION'   , str_contains( $_RAYLIB_CONTENT , 'GenImageCellular' ) );
 
 // Support multiple image editing functions to scale, adjust colors, flip, draw on images, crop...
 // If not defined, still some functions are supported: ImageFormat(), ImageCrop(), ImageToPOT()
-define_default( 'RL_SUPPORT_IMAGE_MANIPULATION' , true );
+define_default( 'RL_SUPPORT_IMAGE_MANIPULATION' , str_contains( $_RAYLIB_CONTENT , 'ImageToPOT' ) );
 
 //------------------------------------------------------------------------------------
 // Module: rtext - Configuration Flags
@@ -1469,45 +1469,65 @@ $RAYLIB_H .= <<<'RAYLIB_H'
 /*RLAPI*/ bool ExportImage(Image image, const char *fileName);                                               // Export image data to file, returns true on success
 /*RLAPI*/ bool ExportImageAsCode(Image image, const char *fileName);                                         // Export image as code file defining an array of bytes, returns true on success
 
-// Image generation functions
-/*RLAPI*/ Image GenImageColor(int width, int height, Color color);                                           // Generate image: plain color
-/*RLAPI*/ Image GenImageGradientV(int width, int height, Color top, Color bottom);                           // Generate image: vertical gradient
-/*RLAPI*/ Image GenImageGradientH(int width, int height, Color left, Color right);                           // Generate image: horizontal gradient
-/*RLAPI*/ Image GenImageGradientRadial(int width, int height, float density, Color inner, Color outer);      // Generate image: radial gradient
-/*RLAPI*/ Image GenImageChecked(int width, int height, int checksX, int checksY, Color col1, Color col2);    // Generate image: checked
-/*RLAPI*/ Image GenImageWhiteNoise(int width, int height, float factor);                                     // Generate image: white noise
-/*RLAPI*/ Image GenImagePerlinNoise(int width, int height, int offsetX, int offsetY, float scale);           // Generate image: perlin noise
-/*RLAPI*/ Image GenImageCellular(int width, int height, int tileSize);                                       // Generate image: cellular algorithm, bigger tileSize means bigger cells
-/*RLAPI*/ Image GenImageText(int width, int height, const char *text);                                       // Generate image: grayscale image from text data
+RAYLIB_H;
+
+	if ( RL_SUPPORT_IMAGE_GENERATION || $_WRAPPER_SCRIPT_RUNNING_IN_UTILITY_MODE_87987987 ) {
+	$RAYLIB_H .= <<<'RAYLIB_H'
+
+	// Image generation functions
+	/*RLAPI*/ Image GenImageColor(int width, int height, Color color);                                           // Generate image: plain color
+	/*RLAPI*/ Image GenImageGradientV(int width, int height, Color top, Color bottom);                           // Generate image: vertical gradient
+	/*RLAPI*/ Image GenImageGradientH(int width, int height, Color left, Color right);                           // Generate image: horizontal gradient
+	/*RLAPI*/ Image GenImageGradientRadial(int width, int height, float density, Color inner, Color outer);      // Generate image: radial gradient
+	/*RLAPI*/ Image GenImageChecked(int width, int height, int checksX, int checksY, Color col1, Color col2);    // Generate image: checked
+	/*RLAPI*/ Image GenImageWhiteNoise(int width, int height, float factor);                                     // Generate image: white noise
+	/*RLAPI*/ Image GenImagePerlinNoise(int width, int height, int offsetX, int offsetY, float scale);           // Generate image: perlin noise
+	/*RLAPI*/ Image GenImageCellular(int width, int height, int tileSize);                                       // Generate image: cellular algorithm, bigger tileSize means bigger cells
+	/*RLAPI*/ Image GenImageText(int width, int height, const char *text);                                       // Generate image: grayscale image from text data
+
+	RAYLIB_H; }
+
+$RAYLIB_H .= <<<'RAYLIB_H'
 
 // Image manipulation functions
 /*RLAPI*/ Image ImageCopy(Image image);                                                                      // Create an image duplicate (useful for transformations)
 /*RLAPI*/ Image ImageFromImage(Image image, Rectangle rec);                                                  // Create an image from another image piece
 /*RLAPI*/ Image ImageText(const char *text, int fontSize, Color color);                                      // Create an image from text (default font)
 /*RLAPI*/ Image ImageTextEx(Font font, const char *text, float fontSize, float spacing, Color tint);         // Create an image from text (custom sprite font)
-/*RLAPI*/ void ImageFormat(Image *image, int newFormat);                                                     // Convert image data to desired format
-/*RLAPI*/ void ImageToPOT(Image *image, Color fill);                                                         // Convert image to POT (power-of-two)
 /*RLAPI*/ void ImageCrop(Image *image, Rectangle crop);                                                      // Crop an image to a defined rectangle
-/*RLAPI*/ void ImageAlphaCrop(Image *image, float threshold);                                                // Crop image depending on alpha value
-/*RLAPI*/ void ImageAlphaClear(Image *image, Color color, float threshold);                                  // Clear alpha channel to desired color
-/*RLAPI*/ void ImageAlphaMask(Image *image, Image alphaMask);                                                // Apply alpha mask to image
-/*RLAPI*/ void ImageAlphaPremultiply(Image *image);                                                          // Premultiply alpha channel
-/*RLAPI*/ void ImageBlurGaussian(Image *image, int blurSize);                                                // Apply Gaussian blur using a box blur approximation
-/*RLAPI*/ void ImageResize(Image *image, int newWidth, int newHeight);                                       // Resize image (Bicubic scaling algorithm)
+/*RLAPI*/ void ImageFormat(Image *image, int newFormat);                                                     // Convert image data to desired format
 /*RLAPI*/ void ImageResizeNN(Image *image, int newWidth,int newHeight);                                      // Resize image (Nearest-Neighbor scaling algorithm)
+/*RLAPI*/ void ImageResize(Image *image, int newWidth, int newHeight);                                       // Resize image (Bicubic scaling algorithm)
 /*RLAPI*/ void ImageResizeCanvas(Image *image, int newWidth, int newHeight, int offsetX, int offsetY, Color fill);  // Resize canvas and fill with color
-/*RLAPI*/ void ImageMipmaps(Image *image);                                                                   // Compute all mipmap levels for a provided image
-/*RLAPI*/ void ImageDither(Image *image, int rBpp, int gBpp, int bBpp, int aBpp);                            // Dither image data to 16bpp or lower (Floyd-Steinberg dithering)
-/*RLAPI*/ void ImageFlipVertical(Image *image);                                                              // Flip image vertically
-/*RLAPI*/ void ImageFlipHorizontal(Image *image);                                                            // Flip image horizontally
-/*RLAPI*/ void ImageRotateCW(Image *image);                                                                  // Rotate image clockwise 90deg
-/*RLAPI*/ void ImageRotateCCW(Image *image);                                                                 // Rotate image counter-clockwise 90deg
-/*RLAPI*/ void ImageColorTint(Image *image, Color color);                                                    // Modify image color: tint
-/*RLAPI*/ void ImageColorInvert(Image *image);                                                               // Modify image color: invert
-/*RLAPI*/ void ImageColorGrayscale(Image *image);                                                            // Modify image color: grayscale
-/*RLAPI*/ void ImageColorContrast(Image *image, float contrast);                                             // Modify image color: contrast (-100 to 100)
-/*RLAPI*/ void ImageColorBrightness(Image *image, int brightness);                                           // Modify image color: brightness (-255 to 255)
-/*RLAPI*/ void ImageColorReplace(Image *image, Color color, Color replace);                                  // Modify image color: replace color
+
+RAYLIB_H;
+
+	if ( RL_SUPPORT_IMAGE_MANIPULATION || $_WRAPPER_SCRIPT_RUNNING_IN_UTILITY_MODE_87987987 ) {
+	$RAYLIB_H .= <<<'RAYLIB_H'
+
+	/*RLAPI*/ void ImageToPOT(Image *image, Color fill);                                                         // Convert image to POT (power-of-two)
+	/*RLAPI*/ void ImageAlphaCrop(Image *image, float threshold);                                                // Crop image depending on alpha value
+	/*RLAPI*/ void ImageAlphaClear(Image *image, Color color, float threshold);                                  // Clear alpha channel to desired color
+	/*RLAPI*/ void ImageAlphaMask(Image *image, Image alphaMask);                                                // Apply alpha mask to image
+	/*RLAPI*/ void ImageAlphaPremultiply(Image *image);                                                          // Premultiply alpha channel
+	/*RLAPI*/ void ImageBlurGaussian(Image *image, int blurSize);                                                // Apply Gaussian blur using a box blur approximation
+	/*RLAPI*/ void ImageMipmaps(Image *image);                                                                   // Compute all mipmap levels for a provided image
+	/*RLAPI*/ void ImageDither(Image *image, int rBpp, int gBpp, int bBpp, int aBpp);                            // Dither image data to 16bpp or lower (Floyd-Steinberg dithering)
+	/*RLAPI*/ void ImageFlipVertical(Image *image);                                                              // Flip image vertically
+	/*RLAPI*/ void ImageFlipHorizontal(Image *image);                                                            // Flip image horizontally
+	/*RLAPI*/ void ImageRotateCW(Image *image);                                                                  // Rotate image clockwise 90deg
+	/*RLAPI*/ void ImageRotateCCW(Image *image);                                                                 // Rotate image counter-clockwise 90deg
+	/*RLAPI*/ void ImageColorTint(Image *image, Color color);                                                    // Modify image color: tint
+	/*RLAPI*/ void ImageColorInvert(Image *image);                                                               // Modify image color: invert
+	/*RLAPI*/ void ImageColorGrayscale(Image *image);                                                            // Modify image color: grayscale
+	/*RLAPI*/ void ImageColorContrast(Image *image, float contrast);                                             // Modify image color: contrast (-100 to 100)
+	/*RLAPI*/ void ImageColorBrightness(Image *image, int brightness);                                           // Modify image color: brightness (-255 to 255)
+	/*RLAPI*/ void ImageColorReplace(Image *image, Color color, Color replace);                                  // Modify image color: replace color
+
+	RAYLIB_H; }
+
+$RAYLIB_H .= <<< 'RAYLIB_H'
+
 /*RLAPI*/ Color *LoadImageColors(Image image);                                                               // Load color data from image as a Color array (RGBA - 32bit)
 /*RLAPI*/ Color *LoadImagePalette(Image image, int maxPaletteSize, int *colorCount);                         // Load colors palette from image as a Color array (RGBA - 32bit)
 /*RLAPI*/ void UnloadImageColors(Color *colors);                                                             // Unload color data loaded with LoadImageColors()
